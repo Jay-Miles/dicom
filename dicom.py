94d79729539d1cc27456c75675171f67c0023fc7
Jay-Miles <jay.miles@addenbrookes.nhs.uk>
2022-01-19 12:54:55 +0000

    initial commit

---
+++ dicom.py
@@ -0,0 +1,6 @@
+#!usr/bin/env python
+
+# Get a DICOM File Format file to deal with
+# Extract data into a numpy array using pydicom
+# Decompress the file using CharPyLS
+# Convert to PNG using pillow
