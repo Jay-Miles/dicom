d037a9b6f944d0967fb601be990705eec4db5530
Jay-Miles <jay.miles@addenbrookes.nhs.uk>
2022-01-20 13:30:38 +0000

    got png files for 9 of 11 dcm files

--- dicom.py
+++ dicom.py
@@ -14,14 +14,14 @@ index.html
 -Using Case 6 as an example
 -Downloaded file is in .tar.bz2 format and is 172,557 KB
 -contains 26 objects - 15 are directories, 11 are (dcm) files
--total size is 1241286528 bytes
+-total file size is 1241286528 bytes
 -dcm files contain the element '(7fe0, 0010) Pixel Data', which is an array
 
 Largest file is 640869820 bytes:
-Case6 [Case6]/20081001 022733 [ - BREAST IMAGING TOMOSYNTHESIS]/Series 73200000 [MG - R CC Breast Tomosynthesis Image]/1.3.6.1.4.1.5962.99.1.2280943358.716200484.1363785608958.647.0.dcm
+1.3.6.1.4.1.5962.99.1.2280943358.716200484.1363785608958.647.0.dcm
 
 Smallest file is 3262956 bytes:
-Case6 [Case6]/20081001 022733 [ - BREAST IMAGING TOMOSYNTHESIS]/Series 72100000 [MG - R CC Tomosynthesis Projection]/1.3.6.1.4.1.5962.99.1.2280943358.716200484.1363785608958.637.0.dcm
+1.3.6.1.4.1.5962.99.1.2280943358.716200484.1363785608958.637.0.dcm
 
 File structure is:
 Case6 [Case6]
@@ -54,19 +54,25 @@ Case6 [Case6]
 """
 
 
+import os
 import tarfile
 import numpy as np
-from pydicom import dcmread
+
+from pathlib import Path
 from PIL import Image
+from pydicom import dcmread
 
 
-def look_at_download():
-    """ Use the tarfile package to examine Case 6 file contents """
+def look_at_download(archive):
+    """ Use the tarfile package to examine contents of a .tar.bz2 archive """
 
-    tar = tarfile.open('MammoTomoUPMC_Case6.tar.bz2', 'r:bz2')
+    # Open archive
+    tar = tarfile.open(archive, 'r:bz2')
 
+    # Define counter variables
     object_count = 0
     file_count = 0
+    total_size = 0
 
     largest_file_size = 0
     largest_file = ''
@@ -74,13 +80,16 @@ def look_at_download():
     smallest_file_size = 650000000
     smallest_file = ''
 
-    total_size = 0
-
+    # Iterate over all objects
     for tarinfo in tar:
+
         object_count += 1
         total_size += tarinfo.size
 
+        # Print name and size of any regular files
         if tarinfo.isreg():
+            file_count += 1
+
             print("File {a} is {b} bytes.".format(
                 a = tarinfo.name,
                 b = tarinfo.size
@@ -94,16 +103,17 @@ def look_at_download():
                 smallest_file_size = tarinfo.size
                 smallest_file = tarinfo.name
 
-            file_count += 1
-
+        # Print the name of any directories
         elif tarinfo.isdir():
             print("Directory {a}".format(a=tarinfo.name))
 
         else:
             print("{a} is not a file or directory.".format())
 
+    # Close the archive
     tar.close()
 
+    # Print info
     print('{a} total items, {b} are files, {c} total bytes'.format(
         a = object_count,
         b = file_count,
@@ -121,57 +131,134 @@ def look_at_download():
         ))
 
 
-def get_smallest_file():
-    """ Pull out a single DICOM file to look at """
+def identify_largest_file(archive):
+    """
+    Use the tarfile package to find the largest file in a .tar.bz2 archive.
+    """
 
-    tar = tarfile.open('MammoTomoUPMC_Case6.tar.bz2', 'r:bz2')
-    tar.extract('Case6 [Case6]/20081001 022733 [ - BREAST IMAGING TOMOSYNTHESIS]/Series 72100000 [MG - R CC Tomosynthesis Projection]/1.3.6.1.4.1.5962.99.1.2280943358.716200484.1363785608958.637.0.dcm')
+    tar = tarfile.open(archive, 'r:bz2')
+
+    largest_file_size = 0
+    largest_file = ''
+
+    for tarinfo in tar:
+        if tarinfo.isreg():
+            if tarinfo.size < largest_file_size:
+                largest_file_size = tarinfo.size
+                largest_file = tarinfo.name
+
+    tar.close()
+
+    return largest_file
+
+
+def identify_smallest_file(archive):
+    """
+    Use the tarfile package to find the smallest file in a .tar.bz2 archive.
+    """
+
+    tar = tarfile.open(archive, 'r:bz2')
+
+    smallest_file_size = 650000000  # must be > biggest file in archive
+    smallest_file = ''
+
+    for tarinfo in tar:
+        if tarinfo.isreg():
+            if tarinfo.size < smallest_file_size:
+                smallest_file_size = tarinfo.size
+                smallest_file = tarinfo.name
+
+    tar.close()
+
+    return smallest_file
+
+
+def extract_all_files(archive):
+    """ Extract all contents of a .tar.bz2 archive into a new directory. """
+
+    tar = tarfile.open(archive, 'r:bz2')
+    tar.extractall()
     tar.close()
 
 
-def single_file_example():
-    """ Look at a single dcm file """
+def smallest_file_example(archive):
+    """
+    Extract the smallest file from a .tar.bz2 archive, convert it to an
+    image array, and save it as a PNG.
+    """
 
-    # Specify the path to file
-    example_file = 'Case6 [Case6]/20081001 022733 [ - BREAST IMAGING TOMOSYNTHESIS]/Series 72100000 [MG - R CC Tomosynthesis Projection]/1.3.6.1.4.1.5962.99.1.2280943358.716200484.1363785608958.637.0.dcm'
+    filepath = identify_smallest_file(archive)
+    print('filepath is: {a}'.format(a = filepath))
+
+    split_path = filepath.split('/')
+    last_path = split_path[-1]
+    filename = last_path[:-4]
+    print('filename is: {a}'.format(a = filename))
 
     # Read in the dataset as binary using pydicom
-    with open(example_file, 'rb') as file_reader:
+    with open(filepath, 'rb') as file_reader:
         dataset = dcmread(file_reader)
 
-    print(dataset)
+    # Get the file's Pixel Data as an array via the pixel_array element
+    image_array = dataset.pixel_array
+    # print(dataset)
+    # print(image_array)
+    # print(image_array.shape)
 
-    # Get the file's pixel data as an array from the pixel_array element
-    np_array = dataset.pixel_array
-    print(np_array)
-    print(np_array.shape)
+    # Create an image using from the array using pillow (PIL)
+    im = Image.fromarray(np.uint8(image_array))
 
-    # Convert the array to a PNG image using pillow (PIL)
-
-    im = Image.fromarray(np.uint8(np_array))
-    im.save('dicom_example.png')
+    # Save the image in .png format
     im.show()
+    im.save('{a}.png'.format(a = filename))
 
 
-def process_all_case_6():
-    """ Extract all contents """
+def process_whole_directory():
+    """
+    Iterate over the contents of a directory, convert each .dcm file to a
+    numpy array, and save as PNG image.
+    """
 
-    # Extract all contents of the Case 6 download
-    tar = tarfile.open('MammoTomoUPMC_Case6.tar.bz2', 'r:bz2')
-    tar.extractall(path='/case_6_contents')
-    tar.close()
+    # Iterate over the extracted contents
+    for root, dirs, files in os.walk('Case6 [Case6]'):
 
-    # Iterate over all objects in /case_6_contents
-    # If the object is a .dcm file,
-    # Read it in as a dataset, get its pixel array, and convert to png
-    # Store in a separate folder
+        # Look only at files
+        for file in files:
+
+            # Look only at .dcm files
+            if file.lower().endswith('.dcm'):
+
+                # Read in the dataset as binary using pydicom
+                filepath = os.path.join(root, file)
+
+                # Read in the dataset as binary using pydicom
+                with open(filepath, 'rb') as file_reader:
+                    dataset = dcmread(file_reader)
+
+                try:  # try to get Pixel Data as array from pixel_array element
+                    image_array = dataset.pixel_array
+                    # print(image_array)
+                    # print(image_array.shape)
+
+                    try:  # try to create image and save as .png
+                        im = Image.fromarray(np.uint8(image_array))
+                        im.save('{a}.png'.format(a = file[:-4]))
+
+                    except TypeError:
+                        continue
+
+                except MemoryError:
+                    continue
 
 
 def main():
-    # look_at_download()
-    # get_smallest_file()
-    single_file_example()
-    # process_all_case_6()
+    archive = 'MammoTomoUPMC_Case6.tar.bz2'  # Downloaded Case 6 archive
+
+    # look_at_download(archive)
+    # extract_smallest_file(archive)
+    # extract_all_files(archive)
+    # smallest_file_example(archive)
+    process_whole_directory()
 
 
 if __name__ == '__main__':
